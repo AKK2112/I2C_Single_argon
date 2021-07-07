209fb2cdad78cfd26c667d64f09309e39f7aaf08
AKK2112 <aleckk18@gmail.com>
2021-07-07 10:33:11 -0500

    comments

--- src/I2C_Single_argon.ino
+++ src/I2C_Single_argon.ino
@@ -2,7 +2,7 @@ SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 #include "Wire.h"
 
-void receiveEvent(int howMany)
+void receiveEvent(int howMany)//everytime a recieve event is called, this function plays out
 {
   char x = Wire1.read();
   Serial.print("slave");
@@ -17,7 +17,7 @@ void receiveEvent(int howMany)
   }
 }
 
-void requestEvent()
+void requestEvent()//everytime a request event is called, this function plays out
 {
   if (digitalRead(D6) == HIGH)
   {
@@ -31,33 +31,33 @@ void requestEvent()
 
 void setup()
 {
-  Wire.begin();      //master
-  Wire1.begin(0x2A); //slave
+  Wire.begin();      //initializes as master
+  Wire1.begin(0x2A); //initializes asslave
   Serial.begin(9600);
 
   pinMode(D7, OUTPUT);
   pinMode(D6, INPUT);
 
-  Wire1.onReceive(receiveEvent);
-  Wire1.onRequest(requestEvent);
+  Wire1.onReceive(receiveEvent); //the recieve event is called out here
+  Wire1.onRequest(requestEvent);//the request event is called here
   while (!Serial.isConnected());
   Serial.println("I2C Master slave available");
 }
 
 void loop()
 {
-  if (Serial.available())
+  if (Serial.available()) //this is the master telling the slave what to do whem a certian value comes through serial
   {
-    char value = Serial.read();
+    char value = Serial.read(); //if the value is 1, a 1 is written to the slave
     if (value == '1')
     {
-      Serial.println("1");
+      Serial.println("1"); 
       Wire.beginTransmission(0x2a);
       Wire.write('1');
       Wire.endTransmission();
     }
 
-    if (value == '0')
+    if (value == '0') //if the value is 0, a 0 is written to the slave
     {
       Serial.println("0");
       Wire.beginTransmission(0x2a);
@@ -65,13 +65,13 @@ void loop()
       Wire.endTransmission();
     }
 
-    if (value == '?')
+    if (value == '?')//if the value is ?, a ? is written to the slave
     {
       Serial.println("?");
       Wire.requestFrom(0x2a, 1);
       while (Wire.available() == 0);
       char state = Wire.read();
-      Serial.print(state);
+      Serial.println(state);
     }
   }
 }
